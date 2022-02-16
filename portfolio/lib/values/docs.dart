ce7b95b7b044786e129716680d11954d74c905f7
Astha Nayak <58568514+nayakastha@users.noreply.github.com>
2022-02-16 20:40:19 +0530

    resume routing changed

--- portfolio/lib/values/docs.dart
+++ portfolio/lib/values/docs.dart
@@ -1,9 +1,6 @@
 part of values;
 
 class DocumentPath {
-  //Docs route
-  static const String docDir = "assets/assets/docs";
-
   //Docs
-  static const String CV = "$docDir/resume.pdf";
+  static const String CV = "/resume.pdf";
 }
